12125e77342fe20b09716e4f33d38c4ed67b8e80
Quang Nguyen <quangnm.work@gmail.com>
2022-05-05 21:49:59 +0700

    update rollup config

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-anonymous-default-export */
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [
+      dts(),
+      del({ hook: "buildEnd", targets: "./dist/dts" }), //<------ New Addition
+    ],
+
+    external: [/\.css$/],
+  },
+];
