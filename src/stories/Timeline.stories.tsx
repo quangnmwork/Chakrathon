785957513dc6d30db314647a8cc5be962a3d0ccf
Quang Nguyen <quangnm.work@gmail.com>
2022-05-10 19:50:34 +0700

    add example storybook

--- src/stories/Timeline.stories.tsx
+++ src/stories/Timeline.stories.tsx
@@ -13,25 +13,44 @@ export default {
   title: 'Timeline'
 };
 
+const CardContent = () => {
+  return (
+    <Flex flexDirection={'column'} color={'black'}>
+      <Text fontWeight={'bold'}>Timeline</Text>
+      <Text>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </Text>
+    </Flex>
+  );
+};
+
 export const DefaultTimeline = () => {
   return (
     <TimelineContainer>
       <TimelineItem>
         <TimelineSeperator>
-          <TimelineDot />
+          <TimelineDot variant={'solid'} />
           <TimelineConnector />
         </TimelineSeperator>
         <TimelineContent>
-          <Text color={'black'}>Hello</Text>
+          <CardContent />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeperator>
-          <TimelineDot />
+          <TimelineDot variant={'solid'} />
           <TimelineConnector />
         </TimelineSeperator>
         <TimelineContent>
-          <Text color={'black'}>Hello</Text>
+          <CardContent />
         </TimelineContent>
       </TimelineItem>
     </TimelineContainer>
@@ -42,20 +61,20 @@ export const DefaultTimelinePosition = () => {
     <TimelineContainer position={'left'}>
       <TimelineItem>
         <TimelineSeperator>
-          <TimelineDot />
+          <TimelineDot variant={'outline'} />
           <TimelineConnector />
         </TimelineSeperator>
         <TimelineContent>
-          <Text color={'black'}>Hello</Text>
+          <CardContent />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeperator>
-          <TimelineDot />
+          <TimelineDot variant={'outline'} />
           <TimelineConnector />
         </TimelineSeperator>
         <TimelineContent>
-          <Text color={'black'}>Hello</Text>
+          <CardContent />
         </TimelineContent>
       </TimelineItem>
     </TimelineContainer>
@@ -70,7 +89,7 @@ export const DefaultTimelineMix = () => {
           <TimelineConnector />
         </TimelineSeperator>
         <TimelineContent>
-          <Text color={'black'}>Hello</Text>
+          <CardContent />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -79,7 +98,7 @@ export const DefaultTimelineMix = () => {
           <TimelineConnector />
         </TimelineSeperator>
         <TimelineContent>
-          <Text color={'black'}>Hello</Text>
+          <CardContent />
         </TimelineContent>
       </TimelineItem>
     </TimelineContainer>
