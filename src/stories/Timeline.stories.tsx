0c63071754845d088bb78c27e16aba3bc33005b9
Quang Nguyen <quangnm.work@gmail.com>
2022-05-10 21:56:39 +0700

    delete some commandline

--- src/stories/Timeline.stories.tsx
+++ src/stories/Timeline.stories.tsx
@@ -33,102 +33,102 @@ const CardContent = () => {
   );
 };
 
-// export const DefaultTimeline = () => {
-//   return (
-//     <TimelineContainer>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot variant={'solid'} />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot variant={'solid'} />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//     </TimelineContainer>
-//   );
-// };
-// export const DefaultTimelinePosition = () => {
-//   return (
-//     <TimelineContainer position={'left'}>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot variant={'outline'} />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot variant={'outline'} />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//     </TimelineContainer>
-//   );
-// };
-// export const DefaultTimelineMix = () => {
-//   return (
-//     <TimelineContainer position={'mix'}>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//     </TimelineContainer>
-//   );
-// };
-// export const DefaultTimelineWithSize = () => {
-//   return (
-//     <TimelineContainer position={'mix'} size={'sm'}>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//       <TimelineItem>
-//         <TimelineSeperator>
-//           <TimelineDot />
-//           <TimelineConnector />
-//         </TimelineSeperator>
-//         <TimelineContent>
-//           <CardContent />
-//         </TimelineContent>
-//       </TimelineItem>
-//     </TimelineContainer>
-//   );
-// };
+export const DefaultTimeline = () => {
+  return (
+    <TimelineContainer>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot variant={'solid'} />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot variant={'solid'} />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+    </TimelineContainer>
+  );
+};
+export const DefaultTimelinePosition = () => {
+  return (
+    <TimelineContainer position={'left'}>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot variant={'outline'} />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot variant={'outline'} />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+    </TimelineContainer>
+  );
+};
+export const DefaultTimelineMix = () => {
+  return (
+    <TimelineContainer position={'mix'}>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+    </TimelineContainer>
+  );
+};
+export const DefaultTimelineWithSize = () => {
+  return (
+    <TimelineContainer position={'mix'} size={'sm'}>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeperator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeperator>
+        <TimelineContent>
+          <CardContent />
+        </TimelineContent>
+      </TimelineItem>
+    </TimelineContainer>
+  );
+};
 export const DefaultTimelineWithInverseContent = () => {
   return (
     <TimelineContainer size={'sm'} position={'mix'}>
